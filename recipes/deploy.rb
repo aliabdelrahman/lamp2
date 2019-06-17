bbefddabe7926c522cd81920faddd85de495025e
aliabdelrahman <aliabdelrahman46@gmail.com>
2019-06-17 17:36:30 +0200

    Update deploy.rb

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -10,11 +10,40 @@ else
 end
 
 
+file "/home/#{app_user}/git_id_rsa" do
+  owner app_user
+  group app_group
+  mode "0700"
+  content app["app_source"]["ssh_key"]
+end
+
+file "/home/#{app_user}/.ssh/config" do
+  owner app_user
+  group app_group
+  mode "0644"
+  content "Host #{node['repository_host']}
+HostName #{node['repository_host']}
+IdentityFile /home/#{app_user}/git_id_rsa
+User git
+
+StrictHostKeyChecking no
+"
+end
+
+package "git" do
+  # workaround for:
+  # WARNING: The following packages cannot be authenticated!
+  # liberror-perl
+  # STDERR: E: There are problems and -y was used without --force-yes
+  options "--force-yes" if node["platform"] == "ubuntu" && node["platform_version"] == "14.04"
+end
+
+
 git app_path do
     repository app["app_source"]["url"]
     revision app["app_source"]["revision"]
   end
 
-service "apach" do
+service "apache" do
   action :reload
 end
