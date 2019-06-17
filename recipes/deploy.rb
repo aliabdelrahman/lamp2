84eb10280c25e12f685f06bce146316ef4a385fb
aliabdelrahman <aliabdelrahman46@gmail.com>
2019-06-17 11:22:18 +0200

    Update deploy.rb

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -1,26 +1,37 @@
 app = search(:aws_opsworks_app).first
-app_path = "/var/www/html/example.com/public_html/#{app['shortname']}"
-
-package "git" do
-  # workaround for:
-  # WARNING: The following packages cannot be authenticated!
-  # liberror-perl
-  # STDERR: E: There are problems and -y was used without --force-yes
-  options "--force-yes" if node["platform"] == "ubuntu" && node["platform_version"] == "18.04"
+app_path = "/var/www/#{app['shortname']}"
+case node[:platform]
+when 'ubuntu', 'debian'
+  app_user = node['deb_app_user']
+  app_group = node['deb_app_group']
+else
+  app_user = node['rpm_app_user']
+  app_group = node['rpm_app_group']
 end
 
+application app_path do
+  #javascript "4"
+  environment.update(app["environment"])
 
-  git app_path do
-    repository app["app_source"]["url"]
-    revision app["app_source"]["revision"]
+#  git app_path do
+#    user app_user
+#    group app_group
+#    repository app["app_source"]["url"]
+#    #revision app["app_source"]["revision"]
+#  end
+
+  execute 'git clone' do
+    user app_user
+    group app_group
+    cwd app_path
+    command "git clone -b #{app['app_source']['revision']} #{app['app_source']['url']} ."
+    creates "#{app_path}/.git"
   end
 
-  npm_install do
-    retries 3
-    retry_delay 10
+  execute 'git reset' do
+    user app_user
+    group app_group
+    cwd app_path
+    command "git reset --hard"
+    ignore_failure true
   end
-
-  npm_start do
-    action [:stop, :enable, :start]
-  end
-end
